f1014cd182487abf936324534d4cc41321f7373a
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-07-25 21:48:21 -0400

    Removed recompose, added hooks

--- app/components/App/component.js
+++ app/components/App/component.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { Switch, Route } from 'react-router-dom'
 import Header from '../Header'
 import Home from '../Home'
+import LoadingIcon from '../LoadingIcon'
+import { GET_POKEMON } from '../../service/getPokemon'
 import { translateComponent } from '../../utils/translate'
 import './index.scss'
 
@@ -10,15 +12,24 @@ const t = translateComponent('App')
 
 const NotFound = () => <Row><Col><h1>{t('notfound')}</h1></Col></Row>
 
-const App = () =>
-  <div className='poke-main'>
-    <Container fluid>
-      <Route path='/' component={Header} />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='' component={NotFound} />
-      </Switch>
-    </Container>
-  </div>
+const App = ({ name, asyncOperationStart, loading }) => {
+  useEffect(() => {
+    asyncOperationStart(GET_POKEMON, 'magikarp')
+  }, [name])
+
+  if (loading) return <LoadingIcon />
+
+  return (
+    <div className='poke-main'>
+      <Container fluid>
+        <Route path='/' component={Header} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='' component={NotFound} />
+        </Switch>
+      </Container>
+    </div>
+  )
+}
 
 export default App
--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,5 +1,5 @@
 import { memo } from 'react'
-import { compose } from 'recompose'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 import Home from './component'
 
