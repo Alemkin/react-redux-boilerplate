e7986bef28571768bc2d9c35cd04de781dff2440
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-30 15:59:31 -0400

    Using newer sw package

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path')
 const EncodingPlugin = require('webpack-encoding-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const { GenerateSW } = require('workbox-webpack-plugin')
 
 const encodingPlugin = new EncodingPlugin({
   encoding: 'UTF-8'
@@ -71,24 +71,7 @@ module.exports = {
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
     }),
-    new SWPrecacheWebpackPlugin({
-      // By default, a cache-busting query parameter is appended to requests
-      // used to populate the caches, to ensure the responses are fresh.
-      // If a URL is already hashed by Webpack, then there is no concern
-      // about it being stale, and the cache-busting can be skipped.
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: 'service-worker.js',
-      logger (message) {
-        if (message.indexOf('Total precache size is') === 0) {
-          // This message occurs for every build and is a bit too noisy.
-          return
-        }
-        console.log(message)
-      },
-      minify: true,
-      navigateFallback: '/',
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
-    }),
+    new GenerateSW(),
     new CopyPlugin({
       patterns: [
         { from: './pwa', to: '' },
